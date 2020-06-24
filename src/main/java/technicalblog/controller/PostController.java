c3a569a8182d8614352b21065df209520d2b9e55
illusionrs <rajeevshukla255@gmail.com>
2020-06-25 00:13:39 +0530

    updated login page and home page

--- src/main/java/technicalblog/controller/PostController.java
+++ src/main/java/technicalblog/controller/PostController.java
@@ -8,26 +8,18 @@ import technicalblog.model.Post;
 import technicalblog.service.PostService;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 @Controller
-public class HomeController {
+public class PostController {
 
     @Autowired
     private PostService postService;
 
-
-    @RequestMapping("/")
-    public String getAllPosts(Model model){
-
-        ArrayList<Post> posts = postService.getAllPosts();
-
-
-
-        model.addAttribute("posts",posts);
-
-        return "index";
-
-
+    @RequestMapping("posts")
+    public String getUserPosts(Model model) {
+        ArrayList<Post> posts = postService.getOnePost();
+        model.addAttribute("posts", posts);
+        return "posts";
     }
+
 }
